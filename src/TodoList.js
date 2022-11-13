e2bd681f20ecb69a622314dac38f9addf5034d31
Abhijit Desai <abhijitno1@yahoo.com>
2022-11-13 15:44:10 -0500

    Corredction for unique todo identification by react

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,6 +4,6 @@ import Todo from "./Todo";
 export default function TodoList({todos, toggleIsCompleted}) {
     return (
         //todos.length
-        todos.map(todo => { return <Todo todoobj= {todo} toggleIsCompleted= {toggleIsCompleted} /> })
+        todos.map(todo => { return <Todo key={todo.id} todoobj= {todo} toggleIsCompleted= {toggleIsCompleted} /> })
     );
 }
