5be59ef158b581758d60aefdb15f5da502367ade
Abhijit Desai <abhijitno1@yahoo.com>
2022-11-13 14:37:39 -0500

    Implemented todo peristance in local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'TodoApp.todos';
+const DEFAULT_TODOS = [
+  {id: 1, name:'To Do 1', isCompleted:false }, 
+  {id: 2, name:'To Do 2', isCompleted:true }
+];
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, name:'To Do 1', isCompleted:false }, 
-    {id: 2, name:'To Do 2', isCompleted:true }
-  ]);
+  
+  const [todos, setTodos] = useState(DEFAULT_TODOS);
 
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    var storedTodos= JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function toggleIsCompleted(todoid) {
     const newTodos = [...todos];
@@ -19,7 +33,7 @@ function App() {
   function handleTodoAdd() {
     if (todoNameRef.current.value != '') {
       const newTodo = {
-        id: todos.length, 
+        id: uuidv4(),  
         name: todoNameRef.current.value,
         isCompleted: false
       };
@@ -28,12 +42,17 @@ function App() {
     todoNameRef.current.value = ''
   }
 
+  function removeCompleted() {
+    const incompleteTodos = todos.filter(find => !find.isCompleted);
+    setTodos(incompleteTodos);
+  }
+
   return (
     <>
       <TodoList todos={todos} toggleIsCompleted={toggleIsCompleted} />
       <input type="text" ref={todoNameRef} /> <br/>
       <button onClick={handleTodoAdd}>Add ToDo</button>
-      <button>Clear Completed</button>
+      <button onClick={removeCompleted} >Clear Completed</button>
       <div>{todos.filter(todo => todo.isCompleted===false).length} left to do</div>
     </>
   );
